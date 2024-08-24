3ebbcec31e987ecb2ac7acb44b938adfa460a4e9
divyavut <vdivyaaws@gmail.com>
2024-08-24 11:48:39 -0400

    Shell script

--- daws-81s/BashScripting/04-variables.sh
+++ daws-81s/BashScripting/04-variables.sh
@@ -3,5 +3,5 @@ PERSON1=$1
 PERSON2=$2
 
 echo "${PERSON1}: Hi ${PERSON2}, How are you?"
-echo "${PERSON2}: Hello ${PERSON1}, I am doing good, what's going on?
+echo "${PERSON2}: Hello ${PERSON1}, I am doing good, what's going on?"
 echo "${PERSON1}: I started learning Shell Script ${PERSON2}"
