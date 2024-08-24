9329d372a935791046670f460feac122cbc52f43
divyavut <vdivyaaws@gmail.com>
2024-08-24 12:40:28 -0400

    Bash scripting

--- daws-81s/BashScripting/06-arrays.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-FRUITS=("APPLE" "GRAPE" "ORANGE" "BANANA")
-echo "First fruit is: ${FRUITS[0]}"
-echo "Second fruit is: ${FRUITS[1]}"
-echo "Third fruit is: ${FRUITS[2]}"
-echo "All fruits are: ${FRUITS[@]}"
---
+++ daws-81s/BashScripting/06.array.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+FRUITS=("APPLE" "GRAPE "BANANA")
+echo "first fruit is: ${FRUITS[0]}"
+echo "second fruit is: ${FRUITS[1]}"
+echo "Thrid fruit is: ${FRUITS[2]}"
+echo "All fruits is: ${FRUITS[@]}"
