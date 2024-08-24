2f3308bbeea64c1e2f51f87c9e7167d723014f1b
divyavut <vdivyaaws@gmail.com>
2024-08-24 11:17:59 -0400

    Variables in shells script

---
+++ daws-81s/BashScripting/03.variables.sh
@@ -0,0 +1,7 @@
+!#/bin/bash
+PERSON1=Ramesh
+PERSON2=Suresh
+
+echo "${PERSON1}: Hi ${PERSON2}, How are you?"
+echo "${PERSON2}: Hello ${PERSON1}, I am doing good, what's going on?
+echo "${PERSON1}: I started learning Shell Script ${PERSON2}"
