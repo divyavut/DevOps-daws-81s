8b802da98c2c3e788d07aa1ca741f353f681500a
divyavut <vdivyaaws@gmail.com>
2024-08-24 11:50:40 -0400

    Shell script

--- daws-81s/BashScripting/02-conversation.sh
+++ daws-81s/BashScripting/02-conversation.sh
@@ -1,4 +1,4 @@
-!#/bin/bash
+#!/bin/bash
 
 echo "Ramesh: Hi Suresh, How are you?"
 echo "Suresh: Hello Ramesh, I am doing good, what's going on?"
--- daws-81s/BashScripting/03.variables.sh
+++ daws-81s/BashScripting/03.variables.sh
@@ -1,4 +1,4 @@
-!#/bin/bash
+#!/bin/bash
 PERSON1=Ramesh
 PERSON2=Suresh
 
