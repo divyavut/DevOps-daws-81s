e2aa0b9e65500a0cd1f43b3ce2787215ca3ffee8
divyavut <vdivyaaws@gmail.com>
2024-08-24 12:30:36 -0400

    BAsh scripting

--- daws-81s/BashScripting/06-arrays.sh
+++ daws-81s/BashScripting/06-arrays.sh
@@ -1,7 +1,6 @@
 #! /bin/bash
-FRUITS=("APPLE "GRAPE" "ORANGE" "BANANA")
-echo "Frist fruit is: ${FRUITS[0]}"
+FRUITS=("APPLE" "GRAPE" "ORANGE" "BANANA")
+echo "First fruit is: ${FRUITS[0]}"
 echo "Second fruit is: ${FRUITS[1]}"
-echo "Thrid fruit is: ${FRUITS[2]}"
+echo "Third fruit is: ${FRUITS[2]}"
 echo "All fruits are: ${FRUITS[@]}"
-
