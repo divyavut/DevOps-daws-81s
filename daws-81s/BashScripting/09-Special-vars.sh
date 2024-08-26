8e614de63fa250a921240811b80b4316f9e8c3c3
divyavut <vdivyaaws@gmail.com>
2024-08-25 22:19:12 -0400

    Bash Scripting

---
+++ daws-81s/BashScripting/09-Special-vars.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "All variables passed to the script: $@"
+echo "Number of varaibles passed: $#"
+echo "Current working directory: $PWD"
+echo "Home Directory of current: $HOME"
+echo "PID of current executing script: $$"
+sleep 100 &
+echo "PID of Background running command: $!"
