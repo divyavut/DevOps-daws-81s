7e53c49a542d1840ffdf011beadc05d21425e86b
divyavut <vdivyaaws@gmail.com>
2024-08-27 13:00:04 -0400

    Bash Scripting

--- daws-81s/BashScripting/16-redirectors.sh
+++ daws-81s/BashScripting/16-redirectors.sh
@@ -17,7 +17,8 @@ N="\e[0m"
 USERID=$(id -u)
 if [ $USERID -ne 0 ]
 then
-    echo -e "$R User should have super user Previllages to run the script $N" &>>$LOG_FILE
+    # echo -e "$R User should have super user Previllages to run the script $N" &>>$LOG_FILE
+    echo -e "$R User should have super user Previllages to run the script $N" | tee -a $LOG_FILE
     exit 1
 fi
 
@@ -34,8 +35,8 @@ USAGE(){
     echo -e "$R USAGE:: $N sudo 16-redirectors.sh package1 package2 ..."
     exit 1
 }
-echo "Script Started execting at: $(date)"
-
+# echo "Script Started execting at: $(date)" &>>$LOG_FILE
+ echo "Script Started execting at: $(date)" | tee -a $LOG_FILE
 if [ $# -eq 0 ]
 then 
     USAGE 
@@ -43,14 +44,16 @@ fi
 #Step2: Check the Package is Already installed
 for package in $@ #($@ holds the argumnets like git, mysql-server, nginx)
 do 
-  dnf list installed $package
+  dnf list installed $package &>>$LOG_FILE
   if [ $? -ne 0 ]
   then
-    echo "$package has to be installed, please install it" &>>$LOG_FILE
-    dnf install $package -y
+    # echo "$package has to be installed, please install it" &>>$LOG_FILE
+    echo "$package has to be installed, please install it" | tee -a $LOG_FILE
+    dnf install $package -y &>>$LOG_FILE
     #Step3: Verify the Package is sucessfully installed or not
     VERIFY_INSTALLATION $? "Installing $package" 
   else
-    echo -e "$package is already $Y installed, nothing to do. $N" &>>$LOG_FILE
+    # echo -e "$package is already $Y installed, nothing to do. $N" &>>$LOG_FILE
+    echo -e "$package is already $Y installed, nothing to do. $N" | tee -a $LOG_FILE
   fi
 done
