285166490f98ec6c48f89b368ccf1a1387cd4052
divyavut <vdivyaaws@gmail.com>
2024-08-27 10:25:07 -0400

    Bash Scripting

--- daws-81s/BashScripting/15-loop.sh
+++ daws-81s/BashScripting/15-loop.sh
@@ -17,10 +17,10 @@ fi
 VERIFY_INSTALLATION() {
  if [ $1 -ne 0 ]
  then 
-    echo "$2 is...$R FAILED $N"
+    echo -e "$2 is...$R FAILED $N"
     exit 1
  else
-    echo "$2 is...$G SUCCESS $N"
+    echo -e "$2 is...$G SUCCESS $N"
  fi
 } 
 #Step2: Check the Package is Already installed
