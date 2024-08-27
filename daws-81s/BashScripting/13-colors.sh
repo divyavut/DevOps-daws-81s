50428f609f079b1014139a59d9c8179916d7abe5
divyavut <vdivyaaws@gmail.com>
2024-08-26 21:49:10 -0400

    Bash Scripting

--- daws-81s/BashScripting/13-colors.sh
+++ daws-81s/BashScripting/13-colors.sh
@@ -5,5 +5,5 @@ YELLOW="\e[33m"
 N="\e[0m"
 
 echo -e "$RED hello world $N is the first program"
-echo -e "$Green divya reddy $N is her First name"
+echo -e "$GREEN "\e[32m" divya reddy $N is her First name"
 echo -e "$YELLOW vutakanti $N is her last name"
