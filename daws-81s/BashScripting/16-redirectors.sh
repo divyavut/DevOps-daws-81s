7ac355a763264e4eb3e5d107859368922913d10c
divyavut <vdivyaaws@gmail.com>
2024-08-27 13:01:53 -0400

    Bash Scripting

--- daws-81s/BashScripting/16-redirectors.sh
+++ daws-81s/BashScripting/16-redirectors.sh
@@ -25,10 +25,12 @@ fi
 VERIFY_INSTALLATION() {
  if [ $1 -ne 0 ]
  then 
-    echo -e "$2 is...$R FAILED $N" &>>$LOG_FILE
+    # echo -e "$2 is...$R FAILED $N" &>>$LOG_FILE
+    echo -e "$2 is...$R FAILED $N" | tee -a $LOG_FILE 
     exit 1
  else
-    echo -e "$2 is...$G SUCCESS $N" &>>$LOG_FILE
+    # echo -e "$2 is...$G SUCCESS $N" &>>$LOG_FILE
+    echo -e "$2 is...$G SUCCESS $N" | tee -a $LOG_FILE 
  fi
 } 
 USAGE(){
