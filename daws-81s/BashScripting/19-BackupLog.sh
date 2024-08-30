08db77344558d51c1544e74f65e9a16d702d34b5
divyavut <vdivyaaws@gmail.com>
2024-08-29 20:45:16 -0400

    Bash Scripting

--- daws-81s/BashScripting/19-BackupLog.sh
+++ daws-81s/BashScripting/19-BackupLog.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
-USERID=$(id -u)
-if [ $USERID -ne 0 ]
-then
-    echo "User should have super user previlages"
-    exit 1
-fi
+# USERID=$(id -u)
+# if [ $USERID -ne 0 ]
+# then
+#     echo "User should have super user previlages"
+#     exit 1
+# fi
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+Y="\e[0m"
 
 SOURCE_DIR=$1
 DEST_DIR=$2
@@ -13,7 +17,7 @@ TIMESTAMP=$(date +%Y-m%-%d-%H-%M-%S)
 DAYS=${3:-14} # IF $3 is empty,default is 14 days
 
 USAGE(){
-    echo "USAGE:: sh backuoLog.sh <source> <destination> <day(optional)>"
+    echo -e "$R $USAGE:: sh backuoLog.sh <source> <destination> <day(optional)> $N"
 }
 
 if [ $# -lt 2 ]
@@ -24,7 +28,7 @@ fi
 
 if [ ! -d $SOURCE_DIR ]
 then
-    echo "source directory does't exit: $SOURCE_DIR"
+    echo -e "source directory does't exit: $R $SOURCE_DIR $N"
     exit 1
 fi
 
@@ -32,25 +36,25 @@ FILES=$(find $SOURCE_DIR -name "*.log" -mtime +14)
 
 if [ -z "$FILES" ]
 then 
-    echo "Files doesn't exit"
+    echo -e " $R Files doesn't exit $N"
     exit 1
 else
-    echo "Files are found"
+    echo -e "$G Files are found $N"
     dnf install zip -y
     ZIP_FILE="$DEST_DIR/app-logs-$TIMESTAMP.zip"
     find $SOURCE_DIR -name "*.log" -mtime +14 | zip "$ZIP_FILE" -@
     if [ -f $ZIP_FILE ]
     then
-        echo "Successfully zipped files older than $DAYS"
+        echo -e "Successfully zipped files older than $G $DAYS $N"
         #Removing files after zipping
         echo "$FILES" | while IFS= read -r file
         do
-            echo "deleting file : $file"
+            echo -e "deleting file :$Y $file $N"
             rm -f $file
             echo "file deleted: $file"
         done 
     else
-        echo "Zipping files is failed"
+        echo -e "Zipping files is $R failed $N"
         exit 1
     fi
 fi
