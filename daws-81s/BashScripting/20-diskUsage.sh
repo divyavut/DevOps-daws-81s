f28282620852038c46bbb15e90346ccbf0745bf3
divyavut <vdivyaaws@gmail.com>
2024-08-30 11:31:42 -0400

    Bash Scripting

--- daws-81s/BashScripting/20-diskUsage.sh
+++ daws-81s/BashScripting/20-diskUsage.sh
@@ -10,8 +10,8 @@ DISK_THRESHOLD=5
 
 while IFS= read -r line
 do
-    DISKUSAGE_PER=$(echo $line | grep xfs | awk -F " " '{print $6F}' | cut -d "%" -f1)
-    PARTITION=$(echo $line | grep xfs | awk -F " " '{print $NF}')
+    DISKUSAGE_PER=$(echo $line | awk -F " " '{print $6F}' | cut -d "%" -f1)
+    PARTITION=$(echo $line | awk -F " " '{print $NF}')
     if [ "$DISKUSAGE_PER" -ge "$DISK_THRESHOLD" ]
     then    
         echo "$PARTITION is using more Disk usage than $DISK_THRESHOLD, curent usage: $DISKUSAGE_PER. please check it"
